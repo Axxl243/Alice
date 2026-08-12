767ac1df826ee66884306bdff9c6ce92f3a25009
Axxl243 <axeleduardoenriquezvega0@gmail.com>
2026-08-12 08:03:02 -0600

    Respaldo

---
+++ Server.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template 
+
+app= Flask(__name__)
+
+@app.route('/')
+def index():
+    return (render_template('index.html'))
+
+
+
+
+if "__main__" == "__name__":
+    app.run(host="0.0.0.0", port=8088, debug=True)
